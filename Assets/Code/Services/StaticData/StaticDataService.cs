3e55e6b99323326b4f443e3f5bb5763b26c09bf8
Vadim <smileycoderwork@gmail.com>
2022-09-30 13:14:57 +0300

    Refactor static data service

--- Assets/Code/Services/StaticData/StaticDataService.cs
+++ Assets/Code/Services/StaticData/StaticDataService.cs
@@ -7,13 +7,14 @@ namespace Code.Services.StaticData
     public class StaticDataService : IStaticData, IInitializable
     {
         private const string DataPlayerDataPath = "Data/Player Data";
-        
+
         private PlayerData _playerData;
-        
-        public void Initialize() => 
+
+        public void Initialize() =>
             LoadData();
 
-        private void LoadData() => 
+        private void LoadData() =>
+            // TODO: add asset service
             _playerData = Resources.Load<PlayerData>(DataPlayerDataPath);
 
         public PlayerData ForPlayer() =>

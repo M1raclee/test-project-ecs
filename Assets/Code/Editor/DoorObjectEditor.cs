4305298fc2150f33ee63514f704e7521c0603838
Vadim <smileycoderwork@gmail.com>
2022-10-02 13:04:01 +0300

    Add door opened/closed position Extend door object editor

--- Assets/Code/Editor/DoorObjectEditor.cs
+++ Assets/Code/Editor/DoorObjectEditor.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEditor;
 using UnityEditor.SceneManagement;
+using UnityEngine;
 
 namespace Code.Editor
 {
@@ -15,10 +16,49 @@ namespace Code.Editor
                 GenerateGuid(door);
         }
 
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            if (GUILayout.Button("Open"))
+                Open();
+            else if (GUILayout.Button("Close"))
+                Close();
+            else if (GUILayout.Button("Set opened position"))
+                SetOpenedPosition();
+            else if (GUILayout.Button("Set closed position"))
+                SetClosedPosition();
+        }
+
+        private void Open()
+        {
+            var door = (DoorObject) target;
+            door.transform.position = door.OpenedPosition;
+        }
+
+        private void Close()
+        {
+            var door = (DoorObject) target;
+            door.transform.position = door.ClosedPosition;
+        }
+
+        private void SetOpenedPosition()
+        {
+            var door = (DoorObject) target;
+            door.OpenedPosition = door.transform.position;
+            EditorUtility.SetDirty(door);
+        }
+        
+        private void SetClosedPosition()
+        {
+            var door = (DoorObject) target;
+            door.ClosedPosition = door.transform.position;
+            EditorUtility.SetDirty(door);
+        }
+
         private void GenerateGuid(DoorObject door)
         {
             door.Guid = Guid.NewGuid().ToString();
-            
+
             EditorUtility.SetDirty(door);
             EditorSceneManager.MarkSceneDirty(door.gameObject.scene);
         }
--- Assets/Code/Markers/DoorObject.cs
+++ Assets/Code/Markers/DoorObject.cs
@@ -3,4 +3,6 @@ using UnityEngine;
 public class DoorObject : MonoBehaviour
 {
     public string Guid;
+    public Vector3 OpenedPosition;
+    public Vector3 ClosedPosition;
 }
